dd62ff78c3652159172339fdb3763910b5097d9c
himanshu gupta <himanshu180400@gmail.com>
2021-08-15 17:03:37 +0530

    Initial commit

---
+++ Etch-A-sketch App.py
@@ -0,0 +1,35 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+def move_forward():
+     tim.forward(10)
+
+
+def turn_right():
+    new_heading = tim.heading() - 10
+    tim.setheading(new_heading)
+
+
+def turn_left():
+     new_heading = tim.heading()+10
+     tim.setheading(new_heading)
+
+def move_reverse():
+    tim.backward(10)
+
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+screen.listen()
+
+screen.onkey(move_forward, "w")
+screen.onkey(move_reverse, "s")
+screen.onkey(turn_left, "l")
+screen.onkey(turn_right, "r")
+screen.onkey(clear, "c")
+screen.exitonclick()
